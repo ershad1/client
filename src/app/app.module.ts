476f9e7fe16a441dd1a918e7e62278ae43b45167
ershad <ershad@gmail.com>
2017-10-30 04:30:46 +0600

    polishing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppMaterialModule } from './app-material/app-material.module';
-import { AppRouterModule } from './app-router/app-router.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppMaterialModule} from './app-material/app-material.module';
+import {AppRouterModule} from './app-router/app-router.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent } from './app.component';
-import { SettingsComponent } from './core/settings/settings.component';
-import { ProfileComponent } from './core/profile/profile.component';
-import { HomeComponent } from './core/home/home.component';
+import {AppComponent} from './app.component';
+import {SettingsComponent} from './core/settings/settings.component';
+import {ProfileComponent} from './core/profile/profile.component';
+import {HomeComponent} from './core/home/home.component';
+import {LoginComponent} from './core/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SettingsComponent,
     ProfileComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule,
     BrowserAnimationsModule,
     AppMaterialModule,
     AppRouterModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
